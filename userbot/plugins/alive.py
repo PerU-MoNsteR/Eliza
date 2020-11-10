cebe33578e170a04276a4f75768d4c6b4282e654
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-10 10:32:00 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -3,23 +3,22 @@ from telethon import events
 from userbot.utils import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "avengers User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@PerU_MoNster"
 PM_IMG = "https://telegra.ph/file/ced30b3600c5a4e6b2d8a.jpg"
-pm_caption = "🔱 **ELIZA Is Online** 🔱\n\n"
+pm_caption = "🔱 **ELIZA is living with her master** 🔱\n\n"
 
 pm_caption += f"🔸🔹 **ɱყ ცơʂʂ**           :   {DEFAULTUSER}\n"
 
 pm_caption += "🔹🔸 тєℓєтнσи νєяѕισи   :   1.15.0 \n"
 
-pm_caption += "🔸🔹 σffι¢ιαℓ ¢нαииєℓ   :   [ᴊᴏɪɴ](https://t.me/eliza_support_channel)\n"
+pm_caption += "🔸🔹 σffι¢ιαℓ ¢нαииєℓ   :   [ᴊᴏɪɴ](https://t.me/Eliza_support_channel)\n"
 
-pm_caption += "🔹🔸 σffι¢ιαℓ gяσυρ     :   [ᴊᴏɪɴ](https://t.me/eliza_support)\n"
+pm_caption += "🔹🔸 σffι¢ιαℓ gяσυρ     :   [ᴊᴏɪɴ](https://t.me/Eliza_support)\n"
 
-pm_caption += "🔸🔹 ℓι¢єиѕє            :   [ӀíϲҽղՏҽ](https://github.com/The-Avengers-leader/Eliza/blob/master/LICENSE)\n"
+pm_caption += "🔸🔹 ℓι¢єиѕє            :   [ӀíϲҽղՏҽ](https://github.com/suhaash02/Eliza/blob/master/LICENSE)\n"
 
-pm_caption += "🔹🔸 ¢σρуяιgнт          :   [ELIZA](https:/The-Avengers-leader/github.com/Eliza)\n"
+pm_caption += "🔹🔸 ¢σρуяιgнт          :   [ELIZA](https:/suhaash02/github.com/Eliza)\n"
 
-pm_caption += " [...▄███▄███▄\n....█████████\n.......▀█████▀\n............▀█▀\n](https://t.me/eliza_suport)\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
