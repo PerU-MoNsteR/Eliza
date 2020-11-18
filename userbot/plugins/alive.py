954f8843e8f2b69bae2cad217d28f5da2a9ce9c7
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-18 08:20:31 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,21 +5,20 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "avengers User"
 PM_IMG = "https://telegra.ph/file/ced30b3600c5a4e6b2d8a.jpg"
-pm_caption = "🔱 **ELIZA Is Online** 🔱\n\n"
+pm_caption = " Eliza is working successfully \n\n"
 
-pm_caption += f"🔸🔹 **ɱყ ცơʂʂ**           :   {DEFAULTUSER}\n"
+pm_caption += f" **••Mу Bσѕѕ••**     :   {DEFAULTUSER}\n"
 
-pm_caption += "🔹🔸 тєℓєтнσи νєяѕισи   :   1.15.0 \n"
+pm_captionn += " **Pутнση Vєяѕιση**  : `3.8.5`\n"
 
-pm_caption += "🔸🔹 σffι¢ιαℓ ¢нαииєℓ   :   [ᴊᴏɪɴ](https://t.me/eliza_support_channel)\n"
+pm_caption += " **тєℓєтнσи νєяѕισи** :   1.15.0 \n"
 
-pm_caption += "🔹🔸 σffι¢ιαℓ gяσυρ     :   [ᴊᴏɪɴ](https://t.me/eliza_support)\n"
+pm_caption += " **σffι¢ιαℓ ¢нαииєℓ** :   [ᴊᴏɪɴ](https://t.me/eliza_support_channel)\n"
 
-pm_caption += "🔸🔹 ℓι¢єиѕє            :   [ӀíϲҽղՏҽ](https://github.com/suhaash02/Eliza/blob/master/LICENSE)\n"
+pm_caption += " σffι¢ιαℓ gяσυρ       :   [ᴊᴏɪɴ](https://t.me/eliza_support)\n"
 
-pm_caption += "🔹🔸 ¢σρуяιgнт          :   [ELIZA](https:/suhaash02/github.com/Eliza)\n"
+pm_caption += " ℓι¢єиѕє              :   [ӀíϲҽղՏҽ](https://github.com/suhaash02/Eliza/blob/master/LICENSE)\n"
 
-pm_caption += " [...▄███▄███▄\n....█████████\n.......▀█████▀\n............▀█▀\n](https://t.me/eliza_suport)\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
