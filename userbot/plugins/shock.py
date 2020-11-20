90c0fbf6c309df3c418580793ed474afe2888df4
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-20 13:35:22 +0530

    Create shock.py

---
+++ userbot/plugins/shock.py
@@ -0,0 +1,35 @@
+"""Emoji
+Available Commands:
+.shock"""
+
+from telethon import events
+import asyncio
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd("(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(0, 11)
+    input_str = event.pattern_match.group(1)
+    if input_str == "shock":
+        await event.edit(input_str)
+        animation_chars = [
+            "ME: 😂",
+            "ME: 😄",
+            "ME: 😀",
+            "ME: 🙂",
+            "ME: 😐",
+            "ME: 🙁",
+            "ME: 😟",
+            "ME: 😧",
+            "ME: 😨",
+            "ME: 😰",
+            "ME: 😱"
+        ]
+
+        for i in animation_ttl:
+        	
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 11])
