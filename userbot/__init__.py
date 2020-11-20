7639c41f1451a57e543740dce4e7058c882435e2
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-20 09:12:26 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -76,7 +76,7 @@ if bool(ENV):
     ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
     AUTONAME = os.environ.get("AUTONAME", None)
     UPSTREAM_REPO_URL = os.environ.get(
-        "UPSTREAM_REPO_URL", "https://github.com/sandy1709/catuserbot.git"
+        "UPSTREAM_REPO_URL", "https://github.com/suhaash02/Eliza"
     )
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
