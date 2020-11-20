945bb5064d6eeb2d0d40c899a068c406c06d7084
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-20 16:46:38 +0530

    Update fork.py

--- userbot/plugins/fork.py
+++ userbot/plugins/fork.py
@@ -1,18 +1,30 @@
-"""COMMAND : .fork"""
-
 from telethon import events
+
 import asyncio
-from userbot.utils import admin_cmd
+
+
+
+
+
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
 async def _(event):
-   if event.fwd_from:
-   return
+
+    if event.fwd_from:
+
+        return
+
     animation_interval = 5
+
     animation_ttl = range(0, 15)
+
     input_str = event.pattern_match.group(1)
+
     if input_str == "fork":
-    await event.edit(input_str)
-    animation_chars = [
+
+        await event.edit(input_str)
+
+        animation_chars = [
         
             "`Your bot is running\n\nTelethon version:` 1.9.0\n`Python:` 3.7.3\n`User:` @peru_monster\n`Database Status: Databases functioning normally!`",
             "`Connecting To github.com...`",
@@ -35,27 +47,3 @@ async def _(event):
 
             await event.edit(animation_chars[i % 15])
 
-            @borg.on(admin_cmd("nope"))
-async def _(event):
-    if event.fwd_from:
-        return
-    animation_interval = 0.1
-    animation_ttl = range(0,36)
-    #input_str = event.pattern_match.group(1)
-   # if input_str == "nope":
-    await event.edit("nope")
-    animation_chars = [
-            "No",
-            "Problem",
-            "Boss",
-            "Yeah ! No problem",
-            "No Problem Boss.lol",
-            "No Problem Boss😇.Lol",
-            "No Problem Man😇.Lol"
-         ]
-            
-
-    for i in animation_ttl:
-        	
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 18])
