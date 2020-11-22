e08e1926dbfa1a5bdb1220aac29892cf96b13d25
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-22 10:45:49 +0530

    Update fileext.py

--- userbot/plugins/fileext.py
+++ userbot/plugins/fileext.py
@@ -1,16 +1,17 @@
 """Get info about a File Extension
 Syntax: .filext EXTENSION"""
-from telethon import events
+
 import requests
 from bs4 import BeautifulSoup
-from ..utils import admin_cmd
+
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
-@borg.on(admin_cmd(pattern="filext (.*)"))
+@bot.on(admin_cmd(pattern="filext (.*)"))
+@bot.on(sudo_cmd(pattern="filext (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("Processing ...")
     sample_url = "https://www.fileext.com/file-extension/{}.html"
     input_str = event.pattern_match.group(1).lower()
     response_api = requests.get(sample_url.format(input_str))
@@ -19,6 +20,16 @@ async def _(event):
         raw_html = response_api.content
         soup = BeautifulSoup(raw_html, "html.parser")
         ext_details = soup.find_all("td", {"colspan": "3"})[-1].text
-        await event.edit("**File Extension**: `{}`\n**Description**: `{}`".format(input_str, ext_details))
+        await edit_or_reply(
+            event,
+            "**File Extension**: `{}`\n**Description**: `{}`".format(
+                input_str, ext_details
+            ),
+        )
     else:
-        await event.edit("https://www.fileext.com/ responded with {} for query: {}".format(status_code, input_str))
+        await edit_or_reply(
+            event,
+            "https://www.fileext.com/ responded with {} for query: {}".format(
+                status_code, input_str
+            ),
+        )
