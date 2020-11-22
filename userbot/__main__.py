afe8ec139c1c83d9aa343e6840ad5f69dc6e03b4
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-22 13:25:17 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -42,10 +42,10 @@ for name in files:
         if shortname.replace(".py", "") not in Config.NO_LOAD:
             load_module(shortname.replace(".py", ""))
 
-LOGS.info("Yay your userbot is officially working.!!!")
+LOGS.info("Efforts never fail, thank @peru_monster for this awesome Eliza userbot!!")
 LOGS.info(
-    "Congratulation, now type .alive to see message if bot is live\n"
-    "If you need assistance, head to "
+    "hurrah all completed now, type .alive to see message if bot is live\n"
+    "for queries contact me here https://t.me/Eliza_userbot_group"
 )
 
 if len(argv) not in (1, 3, 4):
