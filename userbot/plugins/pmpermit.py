3a9e92cd7b3266d76604ca21c0cbabaab43aac20
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-23 15:23:32 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -6,9 +6,9 @@ from userbot.uniborgConfig import Config
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon import events, errors, functions, types
-from userbot import ALIVE_NAME, CUSTOM_PMPERMIT
+from . import ALIVE_NAME, CUSTOM_PMPERMIT
 from userbot.utils import admin_cmd
-from userbot import CMD_HELP
+from . import CMD_HELP
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
