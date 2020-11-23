5d254f2f804ae60b72b257e100fa67c553dfb7b6
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-23 14:41:33 +0530

    Update amongus.py

--- userbot/plugins/amongus.py
+++ userbot/plugins/amongus.py
@@ -6,7 +6,7 @@
 import asyncio
 
 from userbot.utils import admin_cmd, edit_or_reply
-from userbot import ALIVE_NAME, CMD_HELP
+from . import ALIVE_NAME, CMD_HELP
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Peru User"
 
