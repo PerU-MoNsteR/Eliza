b732b520651922f034adc8a2c0314fec805000b3
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-11-28 14:14:33 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -12,7 +12,7 @@ from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 from userbot import CMD_HELP, bot
 from userbot.utils import admin_cmd
 
-UPSTREAM_REPO_URL = "https://github.com/suhaash02/Eliza"
+UPSTREAM_REPO_URL = "https://github.com/PerU-MoNsteR/Eliza"
 HEROKU_API_KEY = Var.HEROKU_API_KEY
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 
