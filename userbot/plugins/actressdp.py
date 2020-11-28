e987f7ba78d8a034ae90fc1193e024b2c88e7a21
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-11-28 08:55:47 +0530

    Update actressdp.py

--- userbot/plugins/actressdp.py
+++ userbot/plugins/actressdp.py
@@ -76,3 +76,533 @@ async def main(event):
 
         await asyncio.sleep(600) #Edit this to your required needs
 
+        
+#Made By @MarioDevs Keep Credits If You Are Goanna Kang This Lol
+
+#And Thanks To The Creator Of Autopic This Script Was Made from Snippets From That Script
+
+#Usage .avengersdp Im Not Responsible For Any Ban caused By This
+
+import requests , re , random 
+
+import urllib , os 
+
+from telethon.tl import functions
+
+from datetime import datetime
+
+from PIL import Image, ImageDraw, ImageFont
+
+from userbot.utils import admin_cmd
+
+import asyncio
+
+from time import sleep
+
+COLLECTION_STRING = [
+
+  "indian-actress-wallpapers",
+
+  "latest-bollywood-actress-wallpapers-2018-hd",
+
+  "bollywood-actress-wallpaper",
+
+  "hd-wallpapers-of-bollywood-actress",
+
+  "new-bollywood-actress-wallpaper-2018"
+
+]
+
+async def animepp():
+
+    os.system("rm -rf donot.jpg")
+
+    rnd = random.randint(0, len(COLLECTION_STRING) - 1)
+
+    pack = COLLECTION_STRING[rnd]
+
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+
+    f = re.compile('/\w+/full.+.jpg')
+
+    f = f.findall(pc)
+
+    fy = "http://getwallpapers.com"+random.choice(f)
+
+    print(fy)
+
+    if not os.path.exists("f.ttf"):
+
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+
+@borg.on(admin_cmd(pattern="actressdp ?(.*)"))
+
+async def main(event):
+
+    await event.edit("**Starting Actress Profile Pic...\n\nDone !!! Check Your DP **")
+
+    while True:
+
+        await animepp()
+
+        file = await event.client.upload_file("donottouch.jpg")  
+
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+
+        os.system("rm -rf donottouch.jpg")
+
+        await asyncio.sleep(600)
+
+#Made By  Keep Credits If You Are Goanna Kang This Lol
+
+#And Thanks To The Creator Of Autopic This Script Was Made from Snippets From That Script
+
+#Usage .actressdp Im Not Responsible For Any Ban caused By This
+
+import requests , re , random 
+
+import urllib , os 
+
+from telethon.tl import functions
+
+from datetime import datetime
+
+from PIL import Image, ImageDraw, ImageFont
+
+from userbot.utils import admin_cmd
+
+import asyncio
+
+from time import sleep
+
+COLLECTION_STRING = [
+
+  "pokemon-serena-wallpaper",
+
+  "hd-pokemon-iphone-wallpapers",
+
+  "pokemon-wallpaper-pikachu",
+
+  "doraemon-3d-wallpaper-2018",
+  
+  "pokemon-serena-wallpaper",
+
+  "anime-girls-wallpapers"
+
+]
+
+async def animepp():
+
+    os.system("rm -rf donot.jpg")
+
+    rnd = random.randint(0, len(COLLECTION_STRING) - 1)
+
+    pack = COLLECTION_STRING[rnd]
+
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+
+    f = re.compile('/\w+/full.+.jpg')
+
+    f = f.findall(pc)
+
+    fy = "http://getwallpapers.com"+random.choice(f)
+
+    print(fy)
+
+    if not os.path.exists("f.ttf"):
+
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+
+@borg.on(admin_cmd(pattern="animedp ?(.*)"))
+
+async def main(event):
+
+    await event.edit("**Starting Anime Profile Pic...\n\nDone !!! Check Your DP.🏻**")
+
+    while True:
+
+        await animepp()
+
+        file = await event.client.upload_file("donottouch.jpg")  
+
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+
+        os.system("rm -rf donottouch.jpg")
+
+        await asyncio.sleep(600)
+
+#And Thanks To The Creator Of Autopic This Script Was Made from Snippets From That Script
+
+#Usage .avengersdp Im Not Responsible For Any Ban caused By This
+
+import requests , re , random 
+
+import urllib , os 
+
+from telethon.tl import functions
+
+from datetime import datetime
+
+from PIL import Image, ImageDraw, ImageFont
+
+from userbot.utils import admin_cmd
+
+import asyncio
+
+from time import sleep
+
+COLLECTION_STRING = [
+
+  "avengers-logo-wallpaper",
+
+  "avengers-hd-wallpapers-1080p",
+
+  "avengers-iphone-wallpaper",
+
+  "iron-man-wallpaper-1920x1080",
+
+  "iron-man-wallpapers"
+
+]
+
+async def animepp():
+
+    os.system("rm -rf donot.jpg")
+
+    rnd = random.randint(0, len(COLLECTION_STRING) - 1)
+
+    pack = COLLECTION_STRING[rnd]
+
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+
+    f = re.compile('/\w+/full.+.jpg')
+
+    f = f.findall(pc)
+
+    fy = "http://getwallpapers.com"+random.choice(f)
+
+    print(fy)
+
+    if not os.path.exists("f.ttf"):
+
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+
+@borg.on(admin_cmd(pattern="avengersdp ?(.*)"))
+
+async def main(event):
+
+    await event.edit("**Starting Avengers Profile Pic...\n\nDone !!!**")
+
+    while True:
+
+        await animepp()
+
+        file = await event.client.upload_file("donottouch.jpg")  
+
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+
+        os.system("rm -rf donottouch.jpg")
+
+        await asyncio.sleep(1000)
+        
+import requests , re , random 
+
+import urllib , os 
+
+from telethon.tl import functions
+
+from datetime import datetime
+
+from PIL import Image, ImageDraw, ImageFont
+
+from userbot.utils import admin_cmd
+
+import asyncio
+
+from time import sleep
+
+COLLECTION_STRINGZ = [
+
+  "star-wars-wallpaper-1080p",
+
+  "4k-sci-fi-wallpaper",
+
+  "star-wars-iphone-6-wallpaper",
+
+  "kylo-ren-wallpaper",
+
+  "darth-vader-wallpaper"
+
+]
+
+async def animepp():
+
+    os.system("rm -rf donot.jpg")
+
+    rnd = random.randint(0, len(COLLECTION_STRINGZ) - 1)
+
+    pack = COLLECTION_STRINGZ[rnd]
+
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+
+    f = re.compile('/\w+/full.+.jpg')
+
+    f = f.findall(pc)
+
+    fy = "http://getwallpapers.com"+random.choice(f)
+
+    print(fy)
+
+    if not os.path.exists("f.ttf"):
+
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+
+@borg.on(admin_cmd(pattern="gamerdp ?(.*)"))
+
+async def main(event):
+
+    await event.edit("**Starting Gamer Profile Pic...\n\nDone !!! Check Your DP") #Owner MarioDevs
+
+    while True:
+
+        await animepp()
+
+        file = await event.client.upload_file("donottouch.jpg")  
+
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+
+        os.system("rm -rf donottouch.jpg")
+
+        await asyncio.sleep(60)
+        
+import requests , re , random 
+
+import urllib , os 
+
+from telethon.tl import functions
+
+from datetime import datetime
+
+from PIL import Image, ImageDraw, ImageFont
+
+from userbot.utils import admin_cmd
+
+import asyncio
+
+from time import sleep
+
+COLLECTION_STRING = [
+
+  "avengers-logo-wallpaper",
+
+  "avengers-hd-wallpapers-1080p",
+
+  "avengers-iphone-wallpaper",
+
+  "iron-man-wallpaper-1920x1080",
+
+  "iron-man-wallpapers",
+
+  "Marvel-Shield-iPhone-Wallpaper",
+
+  "Shield-Logo-Wallpaper",
+
+  "Marvel-Shield-Logo-Wallpaper",
+
+  "Agents-of-Shield-Wallpaper",
+
+  "Agents-of-Shield-iPhone-Wallpaper",
+
+  "Agents-of-Shield-Wallpapers-HD"
+
+  "Thor-Wallpaper-1920x1080",
+
+  "Thor-Wallpapers",
+
+  "Avengers-HD-Wallpapers-1080p",
+
+  "Avengers-Wallpaper-for-Desktop",
+
+   "Avengers-4K-Wallpaper",
+
+  "Avengers-Age-of-Ultron-Wallpaper",
+
+  "Avengers-Civil-War-Wallpaper",
+
+  "Avengers-2-Wallpapers",
+
+  "Avengers-Logo-Wallpaper",
+
+  "Marvel-Avengers-Desktop-Wallpaper",
+
+  "4K-Deadpool-Wallpaper",
+
+  "3D-Deadpool-Logo-Wallpaper",
+
+  "Deadpool-HD-Desktop-Wallpaper",
+
+  "Cool-Deadpool-Wallpaper",
+
+  "Thor-Wallpaper-HD"
+  
+]
+
+async def animepp():
+
+    os.system("rm -rf donot.jpg")
+
+    rnd = random.randint(0, len(COLLECTION_STRING) - 1)
+
+    pack = COLLECTION_STRING[rnd]
+
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+
+    f = re.compile('/\w+/full.+.jpg')
+
+    f = f.findall(pc)
+
+    fy = "http://getwallpapers.com"+random.choice(f)
+
+    print(fy)
+
+    if not os.path.exists("f.ttf"):
+
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+
+@borg.on(admin_cmd(pattern="marveldp ?(.*)"))
+
+async def main(event):
+
+    await event.edit("**Starting Marvel Profile Pic's...\n\nDone !!! Check Your DP.**")
+
+    while True:
+
+        await animepp()
+
+        file = await event.client.upload_file("donottouch.jpg")  
+
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+
+        os.system("rm -rf donottouch.jpg")
+
+        await asyncio.sleep(60)
+        
+import requests , re , random 
+import urllib , os 
+from telethon.tl import functions
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from userbot.utils import admin_cmd
+import asyncio
+from time import sleep
+
+COLLECTION_STRING = [
+  "Predator-Wallpapers-Backgrounds",
+  "Alien-vs-Predator-Wallpaper"
+]
+
+async def animepp():
+    os.system("rm -rf donot.jpg")
+    rnd = random.randint(0, len(COLLECTION_STRING) - 1)
+    pack = COLLECTION_STRING[rnd]
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+    f = re.compile('/\w+/full.+.jpg')
+    f = f.findall(pc)
+    fy = "http://getwallpapers.com"+random.choice(f)
+    print(fy)
+    if not os.path.exists("f.ttf"):
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+    
+@borg.on(admin_cmd(pattern="predatordp ?(.*)"))
+async def main(event):
+    await event.edit("**Starting predator Profile Pic.**") #Owner @NihiNivi
+    while True:
+        await animepp()
+        file = await event.client.upload_file("donottouch.jpg")  
+        await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+        os.system("rm -rf donottouch.jpg")
+        await asyncio.sleep(120)
+        
+import requests , re , random 
+
+import urllib , os 
+
+from telethon.tl import functions
+
+from datetime import datetime
+
+from PIL import Image, ImageDraw, ImageFont
+
+from userbot.utils import admin_cmd
+
+import asyncio
+
+from time import sleep
+
+
+# Space lovers 
+COLLECTION_STRINGS = [
+
+  "1920x1080-space-wallpapers",
+
+  "4k-space-wallpaper",
+
+  "cool-space-wallpapers-hd",
+]
+
+async def animepp():
+
+    os.system("rm -rf donot.jpg")
+
+    rnd = random.randint(0, len(COLLECTION_STRINGS) - 1)
+
+    pack = COLLECTION_STRINGS[rnd]
+
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+
+    f = re.compile('/\w+/full.+.jpg')
+
+    f = f.findall(pc)
+
+    fy = "http://getwallpapers.com"+random.choice(f)
+
+    print(fy)
+
+    if not os.path.exists("f.ttf"):
+
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+
+    urllib.request.urlretrieve(fy,"friday.jpg")
+
+@borg.on(admin_cmd(pattern="spacedp ?(.*)"))
+
+async def main(event):
+
+    await event.edit("**Starting Space Profile Pic...\n\nDone !!! Check Your DP") #Owner MarioDevs
+
+    while True:
+
+        await animepp()
+
+        file = await event.client.upload_file("donottouch.jpg")  
+
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+
+        os.system("rm -rf donottouch.jpg")
+
+        await asyncio.sleep(3600)
+        
+
+
