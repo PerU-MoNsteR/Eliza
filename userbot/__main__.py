594f02c18f991ba4383f32a36f321f8d81202013
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-04 14:35:42 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,4 +1,4 @@
-import glob
+"""import glob
 from pathlib import Path
 from sys import argv
 
@@ -8,9 +8,95 @@ from telethon import TelegramClient
 from . import LOGS, bot
 from .Config import Config
 from .utils import load_module
+"""
+  
+import os
+from pathlib import Path
+from sys import argv
+from .Config import Config
+
+import telethon.utils
+from telethon import TelegramClient
+from . import LOGS, bot
+
+from .utils import load_module, start_assistant
+from var import Var
+
+LOAD_USERBOT = os.environ.get("LOAD_USERBOT", True)
+LOAD_ASSISTANT = os.environ.get("LOAD_ASSISTANT", True)
 
 
-async def add_bot(bot_token):
+
+
+
+import glob
+
+if LOAD_USERBOT == True:
+    path = "userbot/plugins/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            load_module(shortname.replace(".py", ""))
+else:
+    print("Userbot is Not Loading As U Have Disabled")
+
+if LOAD_ASSISTANT == True:
+    path = "userbot/plugins/assistant/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            start_assistant(shortname.replace(".py", ""))
+else:
+    print("for queries contact me here https://t.me/Elizasupport01")
+
+print("Efforts never fail, thank @peru_monster for this awesome Eliza userbot.")
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""before assisstant
+    async def add_bot(bot_token):
     await bot.start(bot_token)
     bot.me = await bot.get_me()
     bot.uid = telethon.utils.get_peer_id(bot.me)
@@ -42,18 +128,7 @@ for name in files:
         if shortname.replace(".py", "") not in Config.NO_LOAD:
             load_module(shortname.replace(".py", ""))
             
-path = "userbot/plugins/assistant/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        start_assistant(shortname.replace(".py", ""))
 
-print("you assisatant is also ready with your eliza bot.")
-
-if len(argv) not in (1, 3, 4):
-    bot.disconnect()
 
 LOGS.info("Efforts never fail, thank @peru_monster for this awesome Eliza userbot!!")
 LOGS.info(
@@ -66,6 +141,5 @@ if len(argv) not in (1, 3, 4):
 else:
     bot.tgbot = None
     bot.run_until_disconnected()
-
-
+"""
 
