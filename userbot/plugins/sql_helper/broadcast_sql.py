794b2271d4c461e408c66e46e540021e6352e854
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-04 13:56:21 +0530

    Update broadcast_sql.py

--- userbot/plugins/sql_helper/broadcast_sql.py
+++ userbot/plugins/sql_helper/broadcast_sql.py
@@ -1,8 +1,7 @@
-# Credits : @HeisenbergTheDanger And Friday Userbot
 
 from sqlalchemy import Column, String
 
-from jarvis.plugins.sql_helper import BASE, SESSION
+from userbot.plugins.sql_helper import BASE, SESSION
 
 
 class ghdb(BASE):
