fb69e200d3af180959627f9523db5e31a9ee8561
PerU-MoNsteR <71298965+PerU-MoNsteR+@users.noreply.github.com>
2020-12-05 02:44:09 +0000

    pylint: auto fixes

--- userbot/plugins/update.py
+++ userbot/plugins/update.py
@@ -44,6 +44,4 @@ async def _(event):
     end = datetime.now()
     (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - StartTime))
-    await edit_or_reply(
-        event, f"Eliza Has Been Restarted Or Updated {uptime} Ago !"
-    )
+    await edit_or_reply(event, f"Eliza Has Been Restarted Or Updated {uptime} Ago !")
