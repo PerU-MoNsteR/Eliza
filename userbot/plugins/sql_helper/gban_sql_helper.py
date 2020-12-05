44673278fc01da08f66d1405f4d471e4e1361218
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-05 07:42:31 +0530

    Update gban_sql_helper.py

--- userbot/plugins/sql_helper/gban_sql_helper.py
+++ userbot/plugins/sql_helper/gban_sql_helper.py
@@ -1,45 +1,51 @@
-try:
-    from userbot.plugins.sql_helper import BASE, SESSION
-except ImportError:
-    raise AttributeError
 from sqlalchemy import Column, String
+
+from userbot.plugins.sql_helper import BASE, SESSION
 
 
 class GBan(BASE):
     __tablename__ = "gban"
     chat_id = Column(String(14), primary_key=True)
+    reason = Column(String(127))
 
-    def __init__(self, chat_id):
-        self.chat_id = str(chat_id)  # ensure string
+    def __init__(self, chat_id, reason=""):
+        self.chat_id = chat_id
+        self.reason = reason
 
 
 GBan.__table__.create(checkfirst=True)
 
 
-def get_gban():
-    try:
-        return SESSION.query(GBan)
-    finally:
-        SESSION.close()
-
-
-def is_gban(chat_id):
+def is_gbanned(chat_id):
     try:
         return SESSION.query(GBan).filter(GBan.chat_id == str(chat_id)).one()
-    except:
+    except BaseException:
         return None
     finally:
         SESSION.close()
 
 
-def add_chat_gban(chat_id):
-    adder = GBan(str(chat_id))
+def get_gbanuser(chat_id):
+    try:
+        return SESSION.query(GBan).get(str(chat_id))
+    finally:
+        SESSION.close()
+
+
+def catgban(chat_id, reason):
+    adder = GBan(str(chat_id), str(reason))
     SESSION.add(adder)
     SESSION.commit()
 
 
-def remove_chat_gban(chat_id):
+def catungban(chat_id):
     rem = SESSION.query(GBan).get(str(chat_id))
     if rem:
         SESSION.delete(rem)
         SESSION.commit()
+
+
+def get_all_gbanned():
+    rem = SESSION.query(GBan).all()
+    SESSION.close()
+    return rem
