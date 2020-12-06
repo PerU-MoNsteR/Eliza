e041c4121ad1bcfc5c403c13cfe8025c7735f87a
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-06 16:28:17 +0530

    Update time.py

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -41,7 +41,7 @@ async def _(event):
     await borg.send_file(  # pylint:disable=E0602
         event.chat_id,
         required_file_name,
-        caption="Userbot: Powered by @eliza_support",
+        caption="Userbot: Powered by @elizasupport01",
         # Courtesy: @ManueI15
         reply_to=reply_msg_id,
     )
