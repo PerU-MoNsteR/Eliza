5299be0d2e1eb68f8e3f3f097b52e78c8c402aba
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-07 13:26:49 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -115,12 +115,12 @@ async def carbon_api(e):
         await e.client.send_file(
             e.chat_id,
             file,
-            caption="<< Here's your carbon, \n Carbonised by [eliza](https://www.github.com/suhaash02/suhaash02)>> ",
+            caption="<< Here's your carbon, \n By [Eliza](https://www.github.com/PerU-MoNsteR/PerU-MoNsteR)>> ",
             force_document=True,
             reply_to=e.message.reply_to_msg_id,
         )
 
-        os.remove("./FRIDAY.png")
+        os.remove("./ELIZA.png")
 
         driver.quit()
 
