42c4d74bce24545f21beee997908aba47b40d0be
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-07 16:54:29 +0530

    Update and rename auto_bio to auto_bio.py

--- userbot/plugins/auto_bio.py
+++ userbot/plugins/auto_bio.py
@@ -1,30 +1,23 @@
-"""
-.autobio"""
-
-
 import asyncio
 import time
 
 from telethon.errors import FloodWaitError
 from telethon.tl import functions
 
-from userbot.utils import admin_cmd
-
-BIO_MSG = Config.BIO_MSG
-if BIO_MSG is None:
-    BIO_MSG = "I am a pro @elizasupport01"
+from userbot.utils import admin_cmd, eor, sudo_cmd
 
 DEL_TIME_OUT = 60
 
 
-@borg.on(admin_cmd(pattern="autobio"))  # pylint:disable=E0602
+@borg.on(admin_cmd(pattern="autobio"))  
+@borg.on(sudo_cmd(pattern="bio", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     while True:
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M:%S")
-        bio = f"📅{DMY} {BIO_MSG} ⌚️{HM}"
+        bio = f"📅 {DMY} | I am Pro @Elizasupport01 | ⌚️ {HM}"
         logger.info(bio)
         try:
             await borg(
@@ -32,13 +25,16 @@ async def _(event):
                     about=bio
                 )
             )
+            await eor(event, "Autobio Enabled !!")
+            await asyncio.sleep(2)
+            await event.delete()
         except FloodWaitError as ex:
             logger.warning(str(e))
             await asyncio.sleep(ex.seconds)
         # else:
         # logger.info(r.stringify())
         # await borg.send_message(  # pylint:disable=E0602
-        #     Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
-        #     "Successfully Changed Profile Bio"
+        # Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+        # "Successfully Changed Profile Bio"
         # )
         await asyncio.sleep(DEL_TIME_OUT)
