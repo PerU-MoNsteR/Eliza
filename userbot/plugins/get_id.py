ffded64959c23b90f548826eaa306eddfeb8f58e
PerU-MoNsteR <71298965+PerU-MoNsteR+@users.noreply.github.com>
2020-12-07 06:33:49 +0000

    perumonster: auto fixes

--- userbot/plugins/get_id.py
+++ userbot/plugins/get_id.py
@@ -1,6 +1,5 @@
-
-from telethon import events
 from telethon.utils import pack_bot_file_id
+
 from userbot.utils import admin_cmd
 
 
@@ -9,12 +8,20 @@ async def _(event):
     if event.fwd_from:
         return
     if event.reply_to_msg_id:
-        chat = await event.get_input_chat()
+        await event.get_input_chat()
         r_msg = await event.get_reply_message()
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(str(event.chat_id), str(r_msg.sender_id), bot_api_file_id))
+            await event.edit(
+                "Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(
+                    str(event.chat_id), str(r_msg.sender_id), bot_api_file_id
+                )
+            )
         else:
-            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`".format(str(event.chat_id), str(r_msg.sender_id)))
+            await event.edit(
+                "Current Chat ID: `{}`\nFrom User ID: `{}`".format(
+                    str(event.chat_id), str(r_msg.sender_id)
+                )
+            )
     else:
         await event.edit("Current Chat ID: `{}`".format(str(event.chat_id)))
