307e72792fe4f123134c7d54ab68cd59c49cb2a4
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-08 18:26:53 +0530

    Create gdrive_sql.py

---
+++ userbot/plugins/sql_helper/gdrive_sql.py
@@ -0,0 +1,48 @@
+
+from sqlalchemy import Column, String
+
+from userbot.plugins.sql_helper import BASE, SESSION
+
+
+class Gdrive(BASE):
+    __tablename__ = "webgdrive"
+    web = Column(String(50), primary_key=True)
+
+    def __init__(self, web):
+        self.web = web
+
+
+Gdrive.__table__.create(checkfirst=True)
+
+
+def is_folder(folder_id):
+    try:
+        return SESSION.query(Gdrive).filter(Gdrive.web == str(folder_id))
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
+
+
+def gparent_id(folder_id):
+    adder = SESSION.query(Gdrive).get(folder_id)
+    if not adder:
+        adder = Gdrive(folder_id)
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def get_parent_id():
+    try:
+        return SESSION.query(Gdrive).all()
+    except BaseException:
+        return None
+    finally:
+        SESSION.close()
+
+
+def rmparent_id(folder_id):
+    note = SESSION.query(Gdrive).filter(Gdrive.web == folder_id)
+    if note:
+        note.delete()
+        SESSION.commit()
