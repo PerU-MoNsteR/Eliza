c2789c5743b343775eebee287be3bb0bf861b231
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-08 15:40:26 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,17 +5,19 @@ from . import ALIVE_NAME
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@PerU_MoNster"
 PM_IMG = "https://telegra.ph/file/ced30b3600c5a4e6b2d8a.jpg"
 
-pm_caption = " Eliza is working successfully \n\n"
+pm_caption = " Eliza is Ready to Rock \n\n"
 
-pm_caption += f" **••Mу Bσѕѕ••**     :   {DEFAULTUSER}\n"
+pm_caption += "System Status\n"   
 
-pm_caption += " **тєℓєтнσи νєяѕισи** :   1.15.0 \n"
+pm_caption += f" **••Mу Bσѕѕ••**      :   {DEFAULTUSER}\n"
 
-pm_caption += " **σffι¢ιαℓ ¢нαииєℓ** :   [ᴊᴏɪɴ](https://t.me/Eliza_Userbot_Support)\n"
+pm_caption += " **тєℓєтнσи νєяѕισи**  :   1.18.2 \n"
 
-pm_caption += " σffι¢ιαℓ gяσυρ       :   [ᴊᴏɪɴ](https://t.me/Eliza_userbot_group)\n"
+pm_caption += " **σffι¢ιαℓ ¢нαииєℓ**  :   [ᴊᴏɪɴ](https://t.me/Eliza_Userbot_Support)\n"
 
-pm_caption += " ℓι¢єиѕє              :   [ӀíϲҽղՏҽ](https://github.com/suhaash02/Eliza/blob/master/LICENSE)\n"
+pm_caption += " σffι¢ιαℓ gяσυρ        :   [ᴊᴏɪɴ](https://t.me/Eliza_userbot_group)\n"
+
+pm_caption += " ℓι¢єиѕє               :   [ӀíϲҽղՏҽ](https://github.com/suhaash02/Eliza/blob/master/LICENSE)\n"
 
 # @command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
