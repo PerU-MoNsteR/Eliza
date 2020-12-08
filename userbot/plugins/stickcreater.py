28618a30c2b2f90390251824ae200c25312e9ce5
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-08 16:15:49 +0530

    Update stickcreater.py

--- userbot/plugins/stickcreater.py
+++ userbot/plugins/stickcreater.py
@@ -1,6 +1,6 @@
 # Random RGB Sticklet by @PhycoNinja13b
-
 # modified by @UniBorg
+# imported from ppe-remix by @heyworld & @DeletedUser420
 
 import io
 import os
@@ -10,12 +10,36 @@ import textwrap
 from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.types import InputMessagesFilterDocument
 
-from userbot.utils import admin_cmd
+from userbot. import CMD_HELP, bot
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from . import deEmojify, waifutxt
 
 # RegEx by https://t.me/c/1220993104/500653 ( @SnapDragon7410 )
 
 
-@borg.on(admin_cmd(pattern="stcr ?(?:(.*?) \| )?(.*)", outgoing=True))
+@bot.on(admin_cmd(outgoing=True, pattern="sttxt(?: |$)(.*)"))
+async def waifu(animu):
+    text = animu.pattern_match.group(1)
+    reply_to_id = animu.message
+    if animu.reply_to_msg_id:
+        reply_to_id = await animu.get_reply_message()
+    if not text:
+        if animu.is_reply:
+            text = (await animu.get_reply_message()).message
+        else:
+            await edit_or_reply(
+                animu, "`You haven't written any article, Waifu is going away.`"
+            )
+            return
+    text = deEmojify(text)
+    await animu.delete()
+    await waifutxt(text, animu.chat_id, reply_to_id, bot, animu.client)
+
+
+# 12 21 28 30
+
+
+@bot.on(admin_cmd(pattern=r"stcr ?(?:(.*?) \| )?(.*)", outgoing=True))
 async def sticklet(event):
     R = random.randint(0, 256)
     G = random.randint(0, 256)
@@ -27,17 +51,19 @@ async def sticklet(event):
     if not font_file_name:
         font_file_name = ""
     sticktext = event.pattern_match.group(2)
-    if not sticktext and event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        sticktext = reply_message.message
-    elif not sticktext:
-        await event.edit("need something, hmm")
-        return
+    if not sticktext:
+        if event.reply_to_msg_id:
+            reply_message = await event.get_reply_message()
+            sticktext = reply_message.message
+        else:
+            await edit_or_reply(event, "need something, hmm")
+            return
     if event.reply_to_msg_id:
         reply_message = await event.get_reply_message()
     # delete the userbot command,
     # i don't know why this is required
     await event.delete()
+    sticktext = deEmojify(sticktext)
     # https://docs.python.org/3/library/textwrap.html#textwrap.wrap
     sticktext = textwrap.wrap(sticktext, width=10)
     # converts back the list to a string
@@ -45,7 +71,7 @@ async def sticklet(event):
     image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
     draw = ImageDraw.Draw(image)
     fontsize = 230
-    FONT_FILE = await get_font_file(event.client, "@borg.onts", font_file_name)
+    FONT_FILE = await get_font_file(event.client, "@catfonts", font_file_name)
     font = ImageFont.truetype(FONT_FILE, size=fontsize)
     while draw.multiline_textsize(sticktext, font=font) > (512, 512):
         fontsize -= 3
@@ -62,13 +88,13 @@ async def sticklet(event):
     await event.client.send_file(
         event.chat_id,
         image_stream,
-        caption="Created Using Eliza @Eliza_support",
+        caption="webo's Sticklet",
         reply_to=event.message.reply_to_msg_id,
     )
     # cleanup
     try:
         os.remove(FONT_FILE)
-    except:
+    except BaseException:
         pass
 
 
@@ -87,3 +113,15 @@ async def get_font_file(client, channel_id, search_kw=""):
     font_file_message = random.choice(font_file_message_s)
     # download and return the file path
     return await client.download_media(font_file_message)
+
+
+CMD_HELP.update(
+    {
+        "stickerfun": "**Plugin : **`stickerfun`\
+        \n\n**Syntax : **`.sttxt` <your txt>\
+        \n**Usage : **Anime that makes your writing fun.\
+        \n\n**Syntax : **`.stcr` <your txt>\
+        \n**Usage : **your text as sticker\
+    "
+    }
+)
