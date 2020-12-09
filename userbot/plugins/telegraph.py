5cfb34eb0bb6dfd19be2fc3c26410853d5d7fac2
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-09 08:24:25 +0530

    Update telegraph.py

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -2,49 +2,50 @@
 .tele m as reply to a media
 .tele t as reply to a large text"""
 
+
+from userbot.utils import admin_cmd
 import os
 from datetime import datetime
 
 from PIL import Image
 from telegraph import Telegraph, exceptions, upload_file
 
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from . import BOTLOG, BOTLOG_CHATID, CMD_HELP, mention
 
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
 auth_url = r["auth_url"]
 
 
-@borg.on(admin_cmd("tele (m|t) ?(.*)"))
+@bot.on(admin_cmd(pattern="telegraph (media|text) ?(.*)"))
+@bot.on(admin_cmd(pattern="tele(m|t) ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    if Config.PLUGIN_CHANNEL is None:
-        await event.edit(
-            "Please set the required environment variable `PLUGIN_CHANNEL` for this plugin to work"
+    webevent = await edit_or_reply(event, "`processing........`")
+    if not os.path.isdir(Config.TEMP_DIR):
+        os.makedirs(Config.TEMP_DIR)
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            "Created New Telegraph account {} for the current session. \n**Do not give this url to anyone, even if they say they are from Telegram!**".format(
+                auth_url
+            ),
         )
-        return
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    await borg.send_message(
-        Config.PLUGIN_CHANNEL,
-        "Created New Telegraph account {} for the current session. \n**Do not give this url to anyone, even if they say they are from Telegram!**".format(
-            auth_url
-        ),
-    )
     optional_title = event.pattern_match.group(2)
     if event.reply_to_msg_id:
         start = datetime.now()
         r_message = await event.get_reply_message()
         input_str = event.pattern_match.group(1)
-        if input_str == "media":
-            downloaded_file_name = await borg.download_media(
-                r_message, Config.TMP_DOWNLOAD_DIRECTORY
+        if input_str in ["media", "m"]:
+            downloaded_file_name = await event.client.download_media(
+                r_message, Config.TEMP_DIR
             )
             end = datetime.now()
             ms = (end - start).seconds
-            await event.edit(
-                "Downloaded to {} in {} seconds.".format(downloaded_file_name, ms)
+            await webevent.edit(
+                f"`Downloaded to {downloaded_file_name} in {ms} seconds.`"
             )
             if downloaded_file_name.endswith((".webp")):
                 resize_image(downloaded_file_name)
@@ -52,20 +53,21 @@ async def _(event):
                 start = datetime.now()
                 media_urls = upload_file(downloaded_file_name)
             except exceptions.TelegraphException as exc:
-                await event.edit("ERROR: " + str(exc))
+                await webevent.edit("**Error : **" + str(exc))
                 os.remove(downloaded_file_name)
             else:
                 end = datetime.now()
                 ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
-                await event.edit(
-                    "File uploaded by my master.. https://telegra.ph{} ".format(
-                        media_urls[0], (ms + ms_two)
+                await webevent.edit(
+                    "**Uploaded  [here](https://telegra.ph{})\
+                    \n** In {} seconds **.\n**Uploaded by :-** {}".format(
+                        media_urls[0], (ms + ms_two), (mention)
                     ),
                     link_preview=True,
                 )
-        elif input_str == "text":
-            user_object = await borg.get_entity(r_message.from_id)
+        elif input_str in ["text", "t"]:
+            user_object = await event.client.get_entity(r_message.sender_id)
             title_of_page = user_object.first_name  # + " " + user_object.last_name
             # apparently, all Users do not have last_name field
             if optional_title:
@@ -74,8 +76,8 @@ async def _(event):
             if r_message.media:
                 if page_content != "":
                     title_of_page = page_content
-                downloaded_file_name = await borg.download_media(
-                    r_message, Config.TMP_DOWNLOAD_DIRECTORY
+                downloaded_file_name = await event.client.download_media(
+                    r_message, Config.TEMP_DIR
                 )
                 m_list = None
                 with open(downloaded_file_name, "rb") as fd:
@@ -87,18 +89,19 @@ async def _(event):
             response = telegraph.create_page(title_of_page, html_content=page_content)
             end = datetime.now()
             ms = (end - start).seconds
-            await event.edit(
-                "Pasted ser https://telegra.ph/{} in {} seconds.".format(
-                    response["path"], ms
-                ),
+            web = f"https://telegra.ph/{response['path']}"
+            await webevent.edit(
+                f"**➥ Pasted to :-** [Telegraph]({cat})\
+                 \n**➥ Pasted in {ms} seconds .**",
                 link_preview=True,
             )
     else:
-        await event.edit(
-            "Reply to a message to get a permanent telegra.ph link. (Inspired by @ControllerBot)"
+        await webevent.edit(
+            "`Reply to a message to get a permanent telegra.ph link. (Inspired by @ControllerBot)`",
         )
 
 
 def resize_image(image):
     im = Image.open(image)
     im.save(image, "PNG")
+
