16ca24fa94da2d4db729ffd549e287ae67c7943c
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-09 18:11:44 +0530

    Update forkanim.py

--- userbot/plugins/forkanim.py
+++ userbot/plugins/forkanim.py
@@ -91,3 +91,29 @@ async def payf(event):
         paytext * 2,
     )
     await event.edit(pay)
+    
+from userbot.uniborgConfig import Config
+from userbot.utils import admin_cmd
+
+
+@borg.on(admin_cmd(pattern="frwd"))
+async def _(event):
+    if event.fwd_from:
+        return
+    if Config.PRIVATE_CHANNEL_BOT_API_ID is None:
+        await event.edit(
+            "Please set the required environment variable `PRIVATE_CHANNEL_BOT_API_ID` for this plugin to work"
+        )
+        return
+    try:
+        e = await borg.get_entity(Config.PRIVATE_CHANNEL_BOT_API_ID)
+    except Exception as e:
+        await event.edit(str(e))
+    else:
+        re_message = await event.get_reply_message()
+        # https://t.me/telethonofftopic/78166
+        fwd_message = await borg.forward_messages(e, re_message, silent=True)
+        await borg.forward_messages(event.chat_id, fwd_message)
+        await fwd_message.delete()
+        await event.delete()
+
