f3c58eec18f7edfc365f90bc11d38aedf7326a78
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-13 10:21:49 +0530

    lol

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -119,7 +119,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
                 custom.Button.inline(
                     "⫷⋉", data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                custom.Button.inline("Close", data="close"),
+                custom.Button.inline(" ༒Close ༒", data="close"),
                 custom.Button.inline(
                     "⋊⫸", data="{}_next({})".format(prefix, modulo_page)
                 ),
