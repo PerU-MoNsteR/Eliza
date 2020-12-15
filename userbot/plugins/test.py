066ee286decbc90174ec3b1b2ec98d53be7affae
Charlie Puth <71298965+PerU-MoNsteR@users.noreply.github.com>
2020-12-15 16:55:23 +0530

    Delete test.py

--- userbot/plugins/test.py
+++
@@ -1,8 +0,0 @@
-from uniborg.util import admin_cmd
-
-
-@borg.on(admin_cmd(pattern=r"test"))
-async def test(event):
-    if event.fwd_from:
-        return
-    await event.edit("Test Successfull. Boss !")
